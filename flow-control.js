c0ac02301750482bef7fce0b47d08bd4cc1c7723
Zavier Berolo <zavier.berolo@gmail.com>
2020-04-26 02:08:14 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,3 +14,17 @@ function teenager(age){
     return("You are not a teenager");
   }
 }
+
+
+function ageChecker(age){
+  if(age>12 && age<20){
+    return("You are a teenager!")
+  }
+  else if(age <12){
+    return("You are a kid")
+  }
+  else{
+    return("You are a grownup")
+  }
+}
+
